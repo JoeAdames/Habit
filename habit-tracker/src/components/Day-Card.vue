74aea0f58792bb47f5e0296b29c5c895986f5307
Joe Adames <jadames@weatherworksinc.com>
2021-03-18 02:18:37 -0400

    setting up

---
+++ habit-tracker/src/components/Day-Card.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <Display />
+  <Form />
+</div>
+</template>
+<script>
+import Display from '@Display.vue'
+import Form from '@Form.vue'
+export default {
+  name: 'DayCard',
+  components: {
+    Display,
+    Form
+  }
+}
+</script>
+<style>
+</style>
---
+++ habit-tracker/src/components/Display.vue
@@ -0,0 +1,9 @@
+<template>
+</template>
+<script>
+export default {
+  name: 'Display',
+}
+</script>
+<style>
+</style>
